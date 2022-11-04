5dccabf6f2629f78b87f1d74db4ca8c1d1eaf7a4
muhammad imran imran <117444308+Mallu86@users.noreply.github.com>
2022-11-04 12:21:24 +0000

    function 2 partial

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-let num = 0 
+import math
+import random
+num = 0 
+def generateRandomNumber():
+  math.random()
+  random.randint()
+  
+  
+
+
+def diffrenceFromAnwser():
+  if guess > anwser:
+    print("to high")
+  if guess < anwser:
+    print("to low")
+  pass
